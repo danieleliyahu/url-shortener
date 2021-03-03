b1b83b24198147f6d2da0c281cb711810f7e15d6
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-03-03 11:25:44 +0200

    o

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
+app.use(express.urlencoded({extended:false}))
 app.use("/public", express.static(`./public`));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.post("/api/shorturl/new",(req,res)=>{
+  res.send("Hi")
+})
 
 module.exports = app;
