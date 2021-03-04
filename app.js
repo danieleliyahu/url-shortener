9845317432474c3bf2afe5eefb13574fc7e628a6
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-03-04 16:31:16 +0200

    fix router

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const shortid = require('shortid');
 const api = require("./api");
 app.use("/api", api);
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 app.use("/public", express.static(`./public`));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
