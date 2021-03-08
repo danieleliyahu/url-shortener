2bdfae2e5432708f3ebe4b18c80c19aeed33f276
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-03-08 19:38:39 +0200

    main

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const shorturl = require("./shorturl");
-const bodyParser = require('body-parser')
-const api = Router();
-api.use(bodyParser.json());
-
-api.use("/shorturl", shorturl);
-
-module.exports = api;
--- app.js
+++ app.js
@@ -2,16 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const shortid = require('shortid');
-const api = require("./api");
-app.use("/api", api);
-const bodyParser = require('body-parser')
-app.use(bodyParser.json());
+
+app.use(cors());
+
 app.use("/public", express.static(`./public`));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
 module.exports = app;
-
